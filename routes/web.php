d2ad6cd2a7febff9b525826dc0df2dfeaaaeedb7
hoanthinh123 <hoanthinh311@gmail.com>
2024-10-09 17:02:03 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\CheckAuth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +24,29 @@ Route::get('/', function () {
 Route::get('/user', function () {
     return view('admin.index');
 });
+//Admin
+Route::middleware([Authenticate::class, CheckAuth::class])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('users', [AdminUserController::class, 'index'])->name('admin.users.index');
+
+        Route::get('/users/create', [AdminUserController::class, 'create'])->name('admin.posts.create');
+
+        Route::post('/users/create', [AdminUserController::class, 'store'])->name('admin.users.store');
+
+        Route::get('/users/edit/{post}', [AdminUserController::class, 'edit'])->name('admin.users.edit');
+
+        Route::put('/users/edit/{post}', [AdminUserController::class, 'update'])->name('admin.users.update');
+
+        Route::delete('/users/delete/{post}', [AdminUserController::class, 'destroy'])->name('admin.users.destroy');
+    });
+});
+
+
+//Login, register, logout
+Route::get('/login', [AuthController::class, 'getLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'postLogin'])->name('postLogin');
+
+Route::get('/register', [AuthController::class, 'getRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'postRegister'])->name('postRegister');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
